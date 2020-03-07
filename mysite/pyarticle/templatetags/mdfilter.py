2d6379edc39f0984b2c854a7ed0270a2c86db813
konishi <konickcioc@gmail.com>
2020-03-07 19:56:07 +0900

    ソースコードに行番号が表示されます

--- mysite/pyarticle/templatetags/mdfilter.py
+++ mysite/pyarticle/templatetags/mdfilter.py
@@ -9,18 +9,24 @@ register = template.Library()
 @stringfilter
 def markdown2html(value):
     md = markdown.Markdown(extensions=['tables', 'nl2br', 'fenced_code', 'pymdownx.tilde'])
+
     html = md.convert(value)
 
     lines = html.split("\n")
     result = ""
     id = 0
-    for line in lines:
-        if len(line) > 4:
-            if line[0:4] == "<h1>":
-                new_tag = '<h1 id="tag_{}">'.format(id)
-                id += 1
-                line = new_tag + line[4:]
-        result = result + "\n" + line
-
+    code_id = 0
+    row = 0
+    while row < len(lines):
+        line = lines[row]
+        if len(line) >= 4 and line[0:4] == "<h1>":
+            new_tag = '<h1 id="tag_{}">'.format(id)
+            id += 1
+            line = new_tag + line[4:]
+            result = result + "\n" + line
+        else:
+            result = result + "\n" + line
+        row += 1
+    print(result)
     return result
 
