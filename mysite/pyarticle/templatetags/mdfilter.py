4ad451a2ff55b668b726e9e88d068743b0048b3a
konishi <konickcioc@gmail.com>
2020-10-29 22:38:20 +0900

    ランキング表示を変更しました

--- mysite/pyarticle/templatetags/mdfilter.py
+++ mysite/pyarticle/templatetags/mdfilter.py
@@ -22,6 +22,7 @@ def markdown2html(value):
     markdown_tags.append("tbody")
     markdown_tags.append("td")
 
+
     html = bleach.clean(html, markdown_tags, markdown_attrs)
 
     lines = html.split("\n")
