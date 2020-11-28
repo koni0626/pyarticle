c7f158442a48b6cbd7e93eeb819e21c867c0d505
konishi <konickcioc@gmail.com>
2020-11-28 11:17:18 +0900

    アクセスログの取得制限数を100にしました。

--- mysite/pyarticle/views_admin_access_log.py
+++ mysite/pyarticle/views_admin_access_log.py
@@ -12,9 +12,9 @@ def index(request):
     user = get_user(request.user)
     # マイページには自分の投稿記事とプロフィールを表示する
     if user.is_superuser:
-        logs = AccessLog.objects.all().order_by('id').reverse().all()
+        logs = AccessLog.objects.all().order_by('id').reverse().all()[:100]
     else:
-        logs = AccessLog.objects.filter(user=user).order_by('id').reverse().all()
+        logs = AccessLog.objects.filter(user=user).order_by('id').reverse().all()[:100]
     data = {'logs': logs}
     return custom_render(request, 'pyarticle/admin/access_log/index.html', data)
 
